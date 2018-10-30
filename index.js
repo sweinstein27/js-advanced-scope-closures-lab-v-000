620a0bd9464cb12676d98c0c16071c5f3328a655
Scott Weinstein <sweinstein27@gmail.com>
2018-10-30 14:26:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-function produceDrivingRange(range){
-  if (range < 10){
-    true;
-  } else {
-    false; 
+function produceDrivingRange(blockRange){
+  return function(startingBlock, endingBlock){
+
+    let start = parseInt(startingBlock);
+    let end = parseInt(endingBlock);
+    let distanceToTravel = Math.abs(end - start);
+    let difference = blockRange - distanceToTravel;
+
+    if( difference > 0){
+     return `within range by ${difference}`
+   } else {
+     return `${Math.abs(difference)} blocks out of range`
+   }
   }
 }
